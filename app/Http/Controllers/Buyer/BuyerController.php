0ce530692eaa4a2ce73e8bf039d8022a880421d3
Mohammed Shurrab <mohmmedshurrab07@gmail.com>
2023-05-31 14:35:38 +0200

    Created user, seller, buyer controllers

--- app/Http/Controllers/Buyer/BuyerController.php
+++ app/Http/Controllers/Buyer/BuyerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Buyer;
 
 use App\Http\Controllers\Controller;
+use App\Models\Buyer;
 use Illuminate\Http\Request;
 
 class BuyerController extends Controller
@@ -12,23 +13,8 @@ class BuyerController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
+        $buyers = Buyer::has('transactions')->get();
+        return response()->json(['data' =>  $buyers] , 200);
     }
 
     /**
@@ -36,30 +22,9 @@ class BuyerController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $buyer = Buyer::has('transactions')->findOrFail($id);
+        return response()->json(['data' => $buyer] , 200);
+
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -4,31 +4,42 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the resource.  
      */
     public function index()
     {
-        //
+        $users = User::all();
+        return response()->json(['data' => $users] , 200);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
+ 
+    /**      
+     *    
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        $rules = [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ];
+    
+        $this->validate($request, $rules);
+    
+        $data = $request->all();
+        $data['password'] = bcrypt($request->password);
+        $data['verified'] = User::UNVERIFIED_USER;
+        $data['verification_token'] = User::generateVerificationCode();
+        $data['admin'] = User::REGULAR_USER;
+    
+        $user = User::create($data);
+    
+        return response()->json(['data' => $user], 201);
     }
 
     /**
@@ -36,23 +47,53 @@ class UserController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $user = User::findOrFail($id);
+        return response()->json(['data' => $user], 200);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, string $id)
     {
-        //
+        $user = user::findOrFail($id);
+        $rules = [
+            'email'    => 'email|unique:users,email,' . $user->id,
+            'password' => 'min:6|confirmed',
+            'admin'    => 'in:' . User::ADMIN_USER . ',' . User::REGULAR_USER, 
+        ];
+
+        $this->validate($request , $rules);
+
+        if($request->has('name')){
+            $user->name =  $request->name;
+        } // end if
+        
+        if($request->has('email') && $user->email != $request->email){
+            $user->verified = User::UNVERIFIED_USER;
+            $user->verification_token = User::generateVerificationCode();
+            $user->email = $request->email;
+        } // end if
+
+        if($request->has('password')){
+            $user->password = bcrypt($request->password); 
+        } // end if
+
+        if($request->has('admin') ){
+            if(!$user->isVerified()){
+                return response()->json(['error' => 'Only verified users can modify the admin field' , 'code' => 409] , 409);
+            }
+
+            $user->admin = $request->admin;
+        } // end if
+
+        if(!$user->isDirty()){
+            return response()->json(['error' => 'You need to specify a different value to update' , 'code' => 422] , 422);
+        }
+
+        $user->save();
+        return response()->json(['data' => $user], 200);
     }
 
     /**
@@ -60,6 +101,9 @@ class UserController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $user = User::findOrFail($id);
+        $user->delete();
+        
+        return response()->json(['data' => $user], 200);
     }
 }
